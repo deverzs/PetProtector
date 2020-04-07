f05ae223c37d51297552d5250168ed56ac86b914
Zsuzsanna Dianovics <31230409+deverzs@users.noreply.github.com>
2020-04-06 20:00:26 -0700

    Initial commit

---
+++ app/src/main/java/edu/miracosta/cs134/petprotector/PetListActivity.java
@@ -0,0 +1,14 @@
+package edu.miracosta.cs134.petprotector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PetListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_list);
+    }
+}
